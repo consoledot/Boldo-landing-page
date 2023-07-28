20153f871e3d724dcc580f4b9dd869bb35dcac67
Abimbola Oladele <bimbooladele@outlook.com>
2023-07-28 05:39:36 +0100

    update script strategy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-
+import Script from "next/script";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Script
+        src="https://cdn.tailwindcss.com"
+        strategy="beforeInteractive"
+      ></Script>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
